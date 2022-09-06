071b3586bec2d9798b9dae16dc88a067ab686d52
Juan Hdez <juanh7522@gmail.com>
2022-09-06 09:43:50 -0500

    inicial

---
+++ Practica1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class Token {
+	public:
+		char nombre;
+		char atributo;
+};
+
+
+int main(int argc, char const *argv[])
+{
+	
+	
+
+
+	return 0;
+};

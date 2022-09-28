b27670a57a61f1306eb18ee7e7fb2c26ffecdaf8
AhrisuOdelagt <arisuodelagtv@gmail.com>
2022-09-28 15:28:12 -0500

    Update relational-operator.cpp
    Justo ahora ya funcionan los tokens de espacio y los números.
    Con los números hay un pequeño bug que sólo imprime el del final de una cifra, pero sí las reconoce correctamente

--- relational-operator.cpp
+++ relational-operator.cpp
@@ -25,6 +25,8 @@ class Token {
 //Function declarations
 void getTokens(string stringToProcess);  //Prints the tokens, but in the future it should return a list of tokens
 Token getRelationalOperator(char ** input);
+Token getBlankSpace(char ** input);
+Token getNumber(char ** input);
 char * convertToCharArray(string stringToProcess);
 char nextChar(char ** charArray);
 void backtrack(char ** charArray);
@@ -32,7 +34,7 @@ void fail(Token tk);
 void printPointer(string name, char * pointer);
 
 int main(int argc, char const *argv[]){
-	string h ("9<=67 <>5 >=1 >aa=7 6 <");
+	string h ("9<=67.8 <>5 >=1 >aa=7 6 <");
 	
 	getTokens(h);
 
@@ -53,11 +55,29 @@ void getTokens(string stringToProcess){
 		//cout << "s: " << index << endl;
 
 		Token tk = getRelationalOperator(&index);
-		if(tk.name == "RelOp"){
+		/*if(tk.name == "RelOp"){
 			//cout<<tk.name <<" : "<<tk.attrib<<endl;
 		} else {
 		//	nextChar(&index);  // this will be removed when the other token identifiers are implemented
-		}
+		}*/
+		
+		
+		Token tk0 = getBlankSpace(&index);
+		/*if(tk0.name == "Blank"){
+			//cout<<tk0.name <<" : "<<tk0.attrib<<endl;
+		} else {
+		//	nextChar(&index);  // this will be removed when the other token identifiers are implemented
+		}*/
+		
+		
+		Token tk1 = getNumber(&index);
+		/*if(tk1.name == "Blank"){
+			//cout<<tk1.name <<" : "<<tk1.attrib<<endl;
+		} else {
+		//	nextChar(&index);  // this will be removed when the other token identifiers are implemented
+		}*/
+		
+		
 		i++;
 	}
 }
@@ -66,7 +86,7 @@ Token getRelationalOperator(char ** input){
 	char * startOfToken = (*input)+1;
 	char ** index = input;
 	//backtrack(index);  //This is needed because the nextChar function increments the pointer in the state 0
-	
+	//cout<<"\n"<<*index<<endl;	// Remove when done testing
 	char c; 
 	Token tk = Token("RelOp");
 	int state = 0;
@@ -127,7 +147,143 @@ Token getRelationalOperator(char ** input){
 				break;
 		}
 	}
-	cout<<string(startOfToken, *index+1)<< "\t "<< tk.name <<" - "<<tk.attrib<<endl; 
+	if (tk.attrib != "NRO")
+		cout<<string(startOfToken, *index+1)<< "\t "<< tk.name <<" - "<<tk.attrib<<endl;
+	return tk;
+}
+
+Token getBlankSpace(char ** input){
+	char * startOfToken = (*input)-1;
+	char ** index = input;
+	backtrack(index);  //This is needed because the nextChar function increments the pointer in the state 0
+	
+	char c;
+	Token tk = Token("Blankspace");
+	int state = 0;
+	
+	while (tk.attrib == ""){
+		switch (state){
+			case 0:
+				c = nextChar(index);	// delim could be a blankspace, tab or line feed.
+				if (c == 32)
+					state = 1;
+				else{
+					tk.name = "Fail";
+					tk.attrib = "NBS";	// Not a Blank Space.
+					//backtrack(index);
+				}
+				break;
+			case 1:
+				c = nextChar(index);
+				if (c == 32)
+					state = 1;
+				else
+					state = 2;
+				break;
+			case 2:
+				backtrack(index);
+				tk.attrib = "space";
+				break;
+		}		
+	}
+	if (tk.attrib != "NBS")
+		cout<<string(startOfToken+1, *index)<< "\t "<< tk.name <<" - "<<tk.attrib<<endl;
+	return tk;
+}
+
+Token getNumber(char ** input){
+	char * startOfToken = (*input);
+	char ** index = input;
+	backtrack(index);  //This is needed because the nextChar function increments the pointer in the state 0
+	
+	char c ;
+	Token tk = Token("Number");
+	int state = 0;
+	
+	while (tk.attrib == ""){
+		switch (state){
+			case 0:
+				c = nextChar(index);
+				if (c >= 48 && c <= 57)
+					state = 1;
+				else{
+					tk.name = "Fail";
+					tk.attrib = "NNR";	// Not a Number.
+					//backtrack(index);
+				}
+				break;
+			case 1:
+				c = nextChar(index);
+				if (c >= 48 && c <= 57)
+					state = 1;
+				else if (c == '.')
+					state = 2;
+				else if (c == 'E')
+					state = 5;
+				else
+					state = 3;
+				break;
+			case 2:
+				if (c >= 48 && c <= 57)
+					state = 4;
+				else{	// if the token does not follows the structure of a number, then it is unrecognizable.
+					tk.name = "Fail";
+					tk.attrib = "NNR";
+					//backtrack(index);	
+				}
+				break;
+			case 3:
+				backtrack(index);
+				tk.attrib = "number";
+				break;
+			case 4:
+				c = nextChar(index);
+				if (c >= 48 && c <= 57)
+					state = 4;
+				else if (c == 'E')
+					state = 5;
+				else
+					state = 6;
+				break;
+			case 5:
+				c = nextChar(index);
+				if (c == '+' || c == '-')
+					state = 7;
+				else if (c >= 48 && c <= 57)
+					state = 8;
+				else{	// same as case 2.
+					tk.name = "Fail";
+					tk.attrib = "NNR";
+					//backtrack(index);
+				}
+				break;
+			case 6:
+				backtrack(index);
+				tk.attrib = "number";
+				break;
+			case 7:
+				if (c >= 48 && c <= 57)
+					state = 8;
+				else{	// same as case 2 and 5.
+					tk.name = "Fail";
+					tk.attrib = "NNR";
+					//backtrack(index);
+				}
+				break;
+			case 8:
+				if (c >= 48 && c <= 57)
+					state = 8;
+				else
+					state = 9;
+				break;
+			case 9:
+				backtrack(index);
+				tk.attrib = "number";
+				break;
+		}
+	}
+	if (tk.attrib != "NNR")
+		cout<<string(startOfToken, *index+1)<< "\t "<< tk.name <<" - "<<tk.attrib<<endl;
 	return tk;
 }
 
